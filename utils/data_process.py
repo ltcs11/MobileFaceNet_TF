92c8e9b1bfec41638d512bebd2e1360cca3a78cf
ltcs11 <980686291@qq.com>
2018-08-24 14:19:34 +0800

    Add files via upload

--- utils/data_process.py
+++ utils/data_process.py
@@ -17,7 +17,7 @@ def parse_args():
                         help='path to the binary image file')
     parser.add_argument('--idx_path', default='../datasets/faces_ms1m_112x112/train.idx', type=str,
                         help='path to the image index path')
-    parser.add_argument('--tfrecords_file_path', default='../datasets/tfrecords', type=str,
+    parser.add_argument('--tfrecords_file_path', default='../datasets/faces_ms1m_112x112/tfrecords', type=str,
                         help='path to the output of tfrecords file path')
     args = parser.parse_args()
     return args
@@ -147,7 +147,8 @@ def test_tfrecords():
 
 if __name__ == '__main__':
     '''data process'''
-    create_tfrecords()
+    #create_tfrecords()
+    test_tfrecords()
 
 
 
